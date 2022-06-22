def8885ceaece2fb0981705530bad32fa9721d93
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-06-22 09:42:14 -0700

    [yasin]done day7 problems

---
+++ 2_3_sort.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+size=10
+for(( i=0; i<size; i++))
+do
+    array[i]=$(( RANDOM % 900 + 100 ))
+done
+echo "The 10 random three digit numbers are: "
+echo "${array[@]}"
+
+array=($(echo ${array[*]}| tr " " "\n" | sort -n))
+
+
+echo "The sorted array is: "
+echo "${array[@]}"
+
+echo "The second largest element is ${array[-2]}"
+echo "The second smallest element is ${array[1]}"
